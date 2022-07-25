ebe619e34e028139eba4acf0c3894becb9c97fdc
Paloma <casagrande.paloma@gmail.com>
2022-07-24 21:03:15 -0300

    [feat] (get /games) add query string name to get list of games that start with name

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,7 +1,13 @@
 import connection from "../dbStrategy/pgdb.js";
 
 export async function getGames(req, res){
+    const name = req.query.name;
     try {
+        if(name){
+            const gamesByName = await connection.query(`SELECT games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id WHERE games.name ILIKE '${name}%';`);
+            return res.send(gamesByName.rows);    
+        }
+        
         const games = await connection.query(`SELECT games.*, categories.name as "categoryName" FROM games JOIN categories ON games."categoryId" = categories.id;`);
         res.send(games.rows);
     } catch (error) {
@@ -9,3 +15,4 @@ export async function getGames(req, res){
         res.sendStatus(422);
     }
 };
+
