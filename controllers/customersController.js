73dbd64aceb663f239528aa5fc0adcb2907a5560
Paloma <casagrande.paloma@gmail.com>
2022-07-25 08:47:04 -0300

    [feat] (get /customers/:id) add get to /customers/:id and return  the customer in the db (id, name, phone, cpf, birthday) that has that id

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -15,3 +15,21 @@ export async function getCustomers(req, res){
         res.sendStatus(422);
     }
 };
+
+export async function getCustomersById(req, res){
+    const id = parseInt(req.params.id);
+    if(isNaN(id)){
+        return res.sendStatus(400);
+    }
+
+    try {
+        const customersById = await connection.query(`SELECT * FROM customers WHERE customers.id =$1;`, [id]);
+        const customerById = customersById.rows[0];
+        if(!customerById) return res.sendStatus(404);
+        res.send(customerById);
+        
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(422);
+    }
+};
--- routes/customersRouter.js
+++ routes/customersRouter.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import { getCustomers } from '../controllers/customersController.js';
+import { getCustomers, getCustomersById } from '../controllers/customersController.js';
 
 const router = Router();
 
 router.get('/customers', getCustomers);
+router.get('/customers/:id', getCustomersById);
+
 
 
 export default router;
